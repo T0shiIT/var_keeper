56f98db0cc5ab3458b40248075c7c3181d54861e
T0shiIT <toshikek@vk.com>
2026-04-21 20:31:01 +0300

    Исправлен регист букв в интеграционном тесте

--- test/integration-tests.sh
+++ test/integration-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ANSW=$(curl http://127.0.0.1:5000/)
-if [ "$ANSW" != "Hello, World!" ]; then
+if [ "$ANSW" != "Hello, world!" ]; then
     exit 1
 else
     echo "Integration Test PASS"
